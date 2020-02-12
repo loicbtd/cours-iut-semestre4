e17d1730498e11461f6c5fa575e2e3c0338de6ec
loicbtd <loic.bert.marcel@gmail.com>
2020-02-12 16:14:19 +0100

    feat(web): projet update

--- programmation-web/bejeweled/app/functions/bejeweled.js
+++ programmation-web/bejeweled/app/functions/bejeweled.js
@@ -1,5 +1,14 @@
 function initialize_game(){
 
+    // global vars
+    theme = "unicorn"; // available: unicorn
+    card_size = 100;
+    grid_size_x = 8;
+    grid_size_y = 8;
+    number_of_cards = 8;
+    number_of_cards_to_align = 3;
+    model = Array();
+
     // dom elements
     element_container_header = document.getElementById("container-header");
     element_container_grid = document.getElementById("container-grid");
@@ -9,11 +18,9 @@ function initialize_game(){
     document.getElementById("button-high-scores").addEventListener("click", display_high_scores);
     document.getElementById("button-new-game").addEventListener("click", start_game);
 
-    // global vars
-    grid_size_x = 8;
-    grid_size_y = 8;
-    model = Array();
-    
+    // set style
+    document.getElementsByTagName("body")[0].style.backgroundImage = "url('resources/themes/"+theme+"/background.png')";
+
     start_game();
 }
 
@@ -23,26 +30,25 @@ function start_game(){
     update_grid();
 }
 
+function remove_good_cards(){
+    // const
+    // let number_of_removed_cards = 0;
+    //
+    // for (let y = 0; y < x; y++) {
+    //
+    // }
+}
+
 function new_model(){
     model = Array();
     let line_tmp;
     for (let y = 0; y < grid_size_y; y++) {
         line_tmp = Array();
         for (let x = 0; x < grid_size_x; x++) {
-            line_tmp.push((Math.random() * (8 - 1) + 1).toFixed(0));
+            line_tmp.push((Math.random() * (number_of_cards - 1) + 1).toFixed(0));
         }
         model.push(line_tmp);
     }
-    
-    let is_valid = false;
-    
-    do {
-        for (let y = 0; y < model.length - 3; y++) {
-            for (let x = 0; x < model[y].length - 3; x++) {
-                
-            }
-        }
-    } while(!is_valid);
 }
 
 function new_grid(){
@@ -66,7 +72,7 @@ function update_grid() {
 
     for (let y = 0; y < model.length; y++) {
         for (let x = 0; x < model[y].length; x++) {
-            document.getElementById("cell-row-"+y+"-col-"+x).innerHTML= "<img draggable='false' src='resources/images/gemstone-"+model[y][x]+".png' width='60' height='50'>";
+            document.getElementById("cell-row-"+y+"-col-"+x).innerHTML= "<img draggable='false' src='resources/themes/"+theme+"/cards/"+model[y][x]+".png' width='"+card_size+"' height='"+card_size*(5/6)+"'>";
         }
     }
     // for (let y = 0; y < grid_size_y; y++) {
